7e4d7edf1c522f8c7776a7199c9f0adb9b6d6901
tom8u4286 <tom8u4286@gmail.com>
2021-04-15 10:11:35 +0800

    Initial Commit

---
+++ Firebase_PaperScissorsStone2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Firebase_PaperScissorsStone2
+//
+//  Created by 曲奕帆 on 2021/4/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
